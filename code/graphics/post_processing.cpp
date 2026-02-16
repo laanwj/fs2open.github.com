aab195618ae2ef532c52f4fe4221574b5329af87
Mara van der Laan <126646+laanwj@users.noreply.github.com>
2026-02-16 20:26:15 +0100

    Revert "Fix null dereference in gr_lightshafts_enabled"
    This reverts commit 96eb81a851cabf45c33c12bc4fed1d8cdc290140.

--- code/graphics/post_processing.cpp
+++ code/graphics/post_processing.cpp
@@ -266,7 +266,7 @@ bool gr_lightshafts_enabled()
 		return false;
 	}
 
-	if (graphics::Post_processing_manager == nullptr || !graphics::Post_processing_manager->getLightshaftParams().on) {
+	if (!graphics::Post_processing_manager->getLightshaftParams().on) {
 		return false;
 	}
 
